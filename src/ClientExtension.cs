df76cb0aae111ee38f3501313b17954057f30e51
Peter Tasner <petertasner@users.noreply.github.com>
2021-08-29 18:04:11 +0200

    First commit

---
+++ src/ClientExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ganymede.SDK
+{
+    internal partial class Client
+    {
+        private readonly string _apiKey;
+        private readonly string _apiSecret;
+
+        public Client(string apiKey, string apiSecret, string baseUrl, HttpClient httpClient)
+        {
+            _apiKey = apiKey;
+            _apiSecret = apiSecret;
+            BaseUrl = baseUrl;
+            _httpClient = httpClient;
+            _settings = new System.Lazy<Newtonsoft.Json.JsonSerializerSettings>(CreateSerializerSettings);
+        }
+
+        /// <summary>
+        /// Additional preparation to add security signature to header
+        /// </summary>
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+            Sign(request);
+        }
+
+        private HttpRequestMessage Sign(HttpRequestMessage request)
+        {
+            var signature = GenerateSignature(request);
+            request.Headers.Authorization = new AuthenticationHeaderValue("basic", $"{_apiKey}:{signature}");
+            return request;
+        }
+
+        private string GenerateSignature(HttpRequestMessage req)
+        {
+            var msg = $"{req.Content?.Headers?.ContentLength ?? 0}" +
+                $"{req.Method}" +
+                $"{req.RequestUri.AbsolutePath}" +
+                $"{req.RequestUri.Query}"
+                .ToLower();
+
+            using var hmac = new HMACSHA256
+            {
+                Key = Encoding.UTF8.GetBytes(_apiSecret)
+            };
+
+            return Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(msg)));
+        }
+    }
+}
---
+++ src/GanymedeClient.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Ganymede.SDK
+{
+    public class GanymedeClient
+    {
+        private static readonly HttpClient _httpClient = new();
+        private readonly Client _client;
+
+        /// <summary>
+        /// Creates instance of the Ganymede client
+        /// </summary>
+        /// <param name="key">API public key</param>
+        /// <param name="secret">API private secret</param>
+        /// <param name="apiUrl">Defaults to https://api.bloxxon.co</param>
+        public GanymedeClient(string key, string secret, string apiUrl = "https://api.bloxxon.co")
+        {
+            _client = new Client(key, secret, apiUrl, _httpClient);
+        }
+
+        #region Customer
+
+        /// <summary>
+        /// Get list of all customers
+        /// </summary>
+        /// <returns>All customers</returns>
+        public async Task<IEnumerable<CustomerAccountDto>> GetCustomersAsync()
+        {
+            var response = await _client.ApiExternalV1CustomersGetAsync();
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Get customer by customer ID
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer</param>
+        /// <returns>Customer details</returns>
+        public async Task<CustomerAccountDto> GetCustomerAsync(Guid customerId)
+        {
+            var response = await _client.ApiExternalV1CustomersGetAsync(customerId);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Create new customer. Throws exception if email already used or if ID is provided and already used
+        /// </summary>
+        /// <param name="data">All required data for creating a new customer</param>
+        /// <returns>The GUID ID of the newly created customer</returns>
+        public async Task<Guid> CreateCustomerAsync(CreateCustomerAccountDto data)
+        {
+            var response = await _client.ApiExternalV1CustomersPostAsync(data);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Update customer details. Throws exception if email already used or if ID is provided and already used
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer to update</param>
+        /// <param name="data">All updatable data</param>
+        /// <returns>The GUID ID of the updated customer</returns>
+        public async Task<Guid> UpdateCustomerAsync(Guid customerId, UpdateCustomerAccountDto data)
+        {
+            var response = await _client.ApiExternalV1CustomersPutAsync(customerId, data);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Update customer KYC details.
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer to update</param>
+        /// <param name="data">All updatable data</param>
+        public async Task<bool> UpdateCustomerKycDataAsync(Guid customerId, KycDataDto data)
+        {
+            var response = await _client.ApiExternalV1CustomersKycAsync(customerId, data);
+            return response.Data;
+        }
+
+        #endregion
+
+        #region Retail Wallet
+
+        /// <summary>
+        /// Gets all retail wallets for the specified customer
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer</param>
+        /// <returns>List of wallet details if any</returns>
+        public async Task<IEnumerable<RetailWalletDto>> GetRetailWalletsAsync(Guid customerId)
+        {
+            var response = await _client.ApiExternalV1CustomersRetailWalletsGetAsync(customerId);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Created wallets for all supported blockchains for the specific customer. If customer already has wallets no new wallets are created. 
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer for whom wallets are to be created</param>
+        /// <param name="credentials">The customer Passphrase is required. KeyFileContent is optinal (configurable)</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<RetailWalletDto>> CreateRetailWalletsAsync(Guid customerId, SimpleAccessCredentialsDto credentials)
+        {
+            var response = await _client.ApiExternalV1CustomersRetailWalletsPostAsync(customerId, credentials);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Create an Opt In for a specified Tokenized Asset.
+        /// </summary>
+        /// <param name="customerId">The GUID ID of the customer</param>
+        /// <param name="walletId">The reatail wallet GUID ID</param>
+        public async Task<bool> CreateTokenizedAssetOptInAsync(Guid customerId, Guid walletId, RetailWalletOptInDto data)
+        {
+            var response = await _client.ApiExternalV1CustomersRetailWalletsOptInAsync(customerId, walletId, data);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Initiates the retail wallet recovery process and return a one-time usable recovery key
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public async Task<RetailWalletSeedRecoveryDto> InitiateCustomersRetailWalletsRecoveryAsync(Guid customerId)
+        {
+            var response = await _client.ApiExternalV1CustomersRetailWalletsRecoveryPostAsync(customerId);
+            return response.Data;
+        }
+
+        /// <summary>
+        /// Resets the passpharse for a retail-wallet
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<bool> RecoverRetailWalletSeedAccessAsync(Guid customerId, ResetRetailWalletAccessCredentialsDto data)
+        {
+            var response = await _client.ApiExternalV1CustomersRetailWalletsRecoveryPutAsync(customerId, data);
+            return response.Data;
+        }
+
+        #endregion
+    }
+}
