2ff41dc4d8d632b65367b81a10db63b31849ebb6
Peter Tasner <petertasner@users.noreply.github.com>
2021-08-30 23:41:13 +0200

    Manual testing example

---
+++ src/Ganymede.SDK.Tests/ManualTestingExamples.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Ganymede.SDK.Tests
+{
+    [TestFixture]
+    public class ManualTestingExamples
+    {
+        private readonly GanymedeClient _client = 
+            new GanymedeClient("", "", "http://localhost:4447");
+
+        [Test]
+        public async Task CreateRetailWallets()
+        {
+            var customerId = new Guid("2bc4db1d-2da8-42de-a1ae-009d2f368040");
+            var retailId = await _client.CreateRetailWalletsAsync(customerId, new SimpleAccessCredentialsDto
+            {
+                Passphrase = "Bloxxon1234"
+            });
+        }
+    }
+}
